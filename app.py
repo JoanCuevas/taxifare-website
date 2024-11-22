6b44a5d6ca68b244dcd4d0cff6c1564433d31bf5
Joan Cuevas <124358130+JoanCuevas@users.noreply.github.com>
2024-11-22 15:14:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -40,18 +40,15 @@ def get_coordinates(location):
 def get_route(pickup_lat, pickup_lon, dropoff_lat, dropoff_lon):
     try:
         routing_url = (
-            f"https://graphhopper.com/api/1/route?point={pickup_lat},{pickup_lon}"
-            f"&point={dropoff_lat},{dropoff_lon}"
-            f"&profile=car&locale=en&calc_points=true"
-            f"&points_encoded=false&instructions=true&snap_preventions=motorway"
-            f"&key={ROUTING_API_KEY}"
+            f"https://graphhopper.com/api/1/route?"
+            f"point={pickup_lat},{pickup_lon}&point={dropoff_lat},{dropoff_lon}"
+            f"&profile=car&locale=en&calc_points=true&points_encoded=false&key={ROUTING_API_KEY}"
         )
         response = requests.get(routing_url)
         response.raise_for_status()
         data = response.json()
         if "paths" in data and data["paths"]:
-            path = data["paths"][0]
-            route_coords = path["points"]["coordinates"]
+            route_coords = data["paths"][0]["points"]["coordinates"]
             return [(coord[1], coord[0]) for coord in route_coords]  # (lat, lon)
     except Exception as e:
         st.error(f"Error obteniendo la ruta: {e}")
